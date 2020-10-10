9ba9f9f57a5ada9f2b36fef507fd72c25164f6a2
Ivan Ternovyi <ivanternovyi@gmail.com>
2020-10-10 13:13:35 +0300

    add article model

---
+++ backend/app/models/article.rb
@@ -0,0 +1,15 @@
+class Article < ApplicationRecord
+  VALID_ARTICLE_TYPES = [
+    'blog_post',
+    'facebook_post',
+    'tweet'
+  ]
+
+  validates :name, presence: true
+  validates :text, presence: true
+  validates :article_type, inclusion: { in: VALID_ARTICLE_TYPES }
+
+  scope :by_keyword, -> (keyword) { by_name(keyword).or(by_text(keyword)) }
+  scope :by_name, ->(name) { where('articles.name ILIKE ?', "%#{name}%") }
+  scope :by_text, ->(text) { where('articles.text ILIKE ?', "%#{text}%") }
+end
